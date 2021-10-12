482e028b03ea868a35849b56cedfb838aa2dd6c6
Arslan <ali.arslan319@gmail.com>
2021-10-11 23:59:26 -0400

    fix fetch request url string

--- github.js
+++ github.js
@@ -5,6 +5,6 @@ class Github{
     }
     // one get request for the user, another for the repos of the user
     async getUser(user){
-        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}`)
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
     }
 }
