cae9542f6742e570b9bfa92bf803e019b3ccb24a
Arslan <ali.arslan319@gmail.com>
2021-10-11 23:57:36 -0400

    create async get request to retrieve user profile

---
+++ github.js
@@ -0,0 +1,10 @@
+class Github{
+    constructor(){
+        this.client_id = 'cdf181c5b92d1ad19d28'
+        this.client_secret = 'c5bcc721c473774ce185656c621feee8d0f42b61'
+    }
+    // one get request for the user, another for the repos of the user
+    async getUser(user){
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}`)
+    }
+}
