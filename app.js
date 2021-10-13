4a26886a881ec461158186828aeac3e23e6a36ed
Arslan <ali.arslan319@gmail.com>
2021-10-13 01:17:21 -0400

    create repoResponse to fetch user repos with paramters of 5 and first created ascended in class constructor

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
 // instantiating Github
-const github = new Github
+const github = new Github;
 // instantiating ui
-const ui = new UI
+const ui = new UI;
 
 // search input
-const searchUser = document.getElementById('searchUser')
+const searchUser = document.getElementById('searchUser');
 
 // search input event listener
 searchUser.addEventListener('keyup', (event) => {
     // get text input
-    const userText = event.target.value
-    // making sure userText is not blank 
-    if(userText !== ''){
+    const userText = event.target.value;
+  if(userText !== ''){
         // making http call
         github.getUser(userText)
-        .then(data => {
-            if(data.profile.message === 'Not Found'){
-                // show alert if the user is not found. 'Not Found' comes from the console log message
-                ui.showAlert('User Not Found', 'alert alert-danger')
-            } else {
-                // show profile
-                // showProfile takes in a user, this is got from data.profile
-                ui.showProfile(data.profile)
-                console.log(data)
-            }
-        })
-    } else {
-        // clear profile
-        ui.clearProfile()
-    }
-})
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        // show alert if the user is not found. 'Not Found' comes from the console log message
+        ui.showAlert('User not found', 'alert alert-danger');
+      } else {
+        // show profile
+        // showProfile takes in a user, this is got from data.profile
+        ui.showProfile(data.profile);
+      }
+    })
+  } else {
+    // Clear profile
+    ui.clearProfile();
+  }
+}); 
--- github.js
+++ github.js
@@ -2,11 +2,15 @@ class Github{
     constructor(){
         this.client_id = 'cdf181c5b92d1ad19d28'
         this.client_secret = 'c5bcc721c473774ce185656c621feee8d0f42b61'
+        this.repos_count = 5
+        this.repos_sort = 'created: asc'
     }
     // one get request for the user, another for the repos of the user
     async getUser(user){
         // query strings added together for client id and secret together
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+        
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?client_id=${this.client_id}&client_secret=${this.client_secret}`)
 
         const profile = await profileResponse.json()
         
