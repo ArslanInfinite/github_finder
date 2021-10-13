4d594d7612fbdd3a0855a75a8618f52e984d581a
Arslan <ali.arslan319@gmail.com>
2021-10-13 01:24:29 -0400

    create variables to fetch and convert data to json, set repos as a return value for getUser method

--- github.js
+++ github.js
@@ -10,14 +10,18 @@ class Github{
         // query strings added together for client id and secret together
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
         
-        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`)
 
         const profile = await profileResponse.json()
-        
+        const repos = await repoResponse.json()
+
+        console.log(repoResponse)
+
         return {
             // async and await allows for numerous aysnc operations 
             // profile: profile can be reduced to...
-            profile
+            profile, 
+            repos
         }
     }
 }
